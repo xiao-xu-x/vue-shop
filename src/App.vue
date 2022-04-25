3931e655ffd6764bd3b6b960c6aaabbaf8eed557
马建明 <2583922371@qq.com>
2022-04-25 18:51:39 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+body{
+	
+	margin: 0;
+	padding: 0;
+	
+	
+}
+	
+*{
+	text-decoration: none;
+	list-style: none;
+}
+
+</style>
